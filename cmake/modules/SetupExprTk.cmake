35402af2791b016cf63a6fedfcdf00c00d4c533d
Jae-Seung Yeom <yeom2@llnl.gov>
2019-07-23 18:28:38 -0700

    add separate recipe files to download single header only libraries: ExprTk, and Catch2

---
+++ cmake/modules/SetupExprTk.cmake
@@ -0,0 +1,31 @@
+# Download the ExprTk library which consists of a single header file only.
+# Set up the variables EXPRTK_DIR and EXPRTK_HEADER.
+# Create a target for download step EXPRTK-download
+
+set(WCS_HAS_EXPRTK TRUE)
+set(EXPRTK_SOURCE_DIR ${CMAKE_SOURCE_DIR}/external/exprtk)
+set(EXPRTK_HEADER exprtk.hpp)
+
+find_file(EXPRTK ${EXPRTK_HEADER}
+          HINTS ${EXPRTK_SOURCE_DIR} $ENV{EXPRTK_DIR} ${EXPRTK_DIR})
+
+if (EXPRTK)
+  message(STATUS "Found ExprTk: ${EXPRTK}")
+  add_custom_target(EXPRTK-download)
+  unset(EXPRTK_DIR CACHE)
+  get_filename_component(EXPRTK_DIR ${EXPRTK} DIRECTORY CACHE)
+else ()
+  message(STATUS "EXPRTK not found")
+  ExternalProject_Add(EXPRTK
+    GIT_REPOSITORY https://github.com/ArashPartow/exprtk.git
+    SOURCE_DIR "${EXPRTK_SOURCE_DIR}"
+    LOG_DOWNLOAD ON
+    STEP_TARGETS download
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+  )
+  set(EXPRTK_DIR ${EXPRTK_SOURCE_DIR})
+  # To prevent parallel build from simultaneously downloading
+  ExternalProject_Add_StepDependencies(EXPRTK build EXPRTK-download)
+endif ()

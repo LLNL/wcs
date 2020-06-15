b88a6f43570cc8d27246e6ae4cd1a0cd037b472c
Konstantia Georgouli <georgouli1@llnl.gov>
2020-06-15 11:55:49 -0700

    Print out formulas

--- src/utils/loadSBML.cpp
+++ src/utils/loadSBML.cpp
@@ -35,19 +35,17 @@ int main(int argc, char** argv)
   const char* filename = argv[1];
   SBMLReader reader;
   SBMLDocument* document = reader.readSBML(filename);
-  /*Konstantia*/
-  Model* modelk= document->getModel();
-  ListOfCompartments* compartmentsk= modelk->getListOfCompartments();
-  int compSize=compartmentsk->size();
-  ListOfSpecies* speciesk=modelk->getListOfSpecies();
-  int speciesSize=speciesk->size();
-  ListOfReactions* reactionsk=modelk->getListOfReactions();
-  int reactionsSize=reactionsk->size();
-  ListOfParameters* parametersk=modelk->getListOfParameters();
-  int parametersSize=parametersk->size();  
-  ListOfUnitDefinitions* unitsk=modelk->getListOfUnitDefinitions();
-  int unitsSize=unitsk->size();
-  /*Konstantia*/
+  Model* model = document->getModel();
+  ListOfCompartments* compartmentslist = model->getListOfCompartments();
+  int compartmentsSize = compartmentslist->size();
+  ListOfSpecies* specieslist = model->getListOfSpecies();
+  int speciesSize = specieslist->size();
+  ListOfReactions* reactionslist = model->getListOfReactions();
+  int reactionsSize = reactionslist->size();
+  ListOfParameters* parameterslist = model->getListOfParameters();
+  int parametersSize = parameterslist->size();  
+  ListOfUnitDefinitions* unitslist = model->getListOfUnitDefinitions();
+  int unitsSize = unitslist->size();
 
 
   const unsigned num_errors = document->getNumErrors();
@@ -64,160 +62,210 @@ int main(int argc, char** argv)
     std::cout << "SBML file " << filename << ":" << std::endl
               << "\tLevel = " << document->getLevel()
               << "\tVersion = " << document->getVersion()
-	      /*Konstantia*/	   
-	      << "\n\tNum species= "<< modelk->getNumSpecies() /*Konstantia*/
-	      << "\tNum reactions= "<< modelk->getNumReactions() /*Konstantia*/
+	      << "\n\tNum species = " << model->getNumSpecies() 
+	      << "\tNum reactions = " << model->getNumReactions() 
 	      
 	      << "\n\n//Created by WCS"
-	      << "\nmodel *"<< modelk->getId()<<"()"  /*getName or getID*/
-	      << "\n\n  // Compartments and Species:";  //<<compSize;
-	      for(int ic=0; ic<compSize; ic++){
-	      	std::cout << "\n  compartment "<< compartmentsk->get(ic)->getIdAttribute()<<";\n";  /*getName or getID*/
-	      }
-	      std::cout<<"  species";
-	      int cnt=0;
-	      for(int ic=0; ic<speciesSize; ic++){
-		if (!speciesk->get(ic)->getHasOnlySubstanceUnits()){      
-                  if (cnt==0){	            		
-                     std::cout <<" "<<  speciesk->get(ic)->getIdAttribute()<<" in "<<speciesk->get(ic)->getCompartment();  
-		  }else{	             	
-                     std::cout <<", "<<  speciesk->get(ic)->getIdAttribute()<<" in "<<speciesk->get(ic)->getCompartment();  
-		  }
-		  cnt++;
-		}
-	      }
-	      cnt=0;
-	      for(int ic=0; ic<speciesSize; ic++){
-                if (speciesk->get(ic)->getHasOnlySubstanceUnits()){
-                  if (cnt==0){ 
-                     std::cout<<";\n  substanceOnly species";
-                     std::cout <<" $"<<  speciesk->get(ic)->getIdAttribute()<<" in "<<speciesk->get(ic)->getCompartment();
-                  }else{                        
-                     std::cout <<", $"<<  speciesk->get(ic)->getIdAttribute()<<" in "<<speciesk->get(ic)->getCompartment();
-                  }
-		  cnt++;
-                }
-              }
-	      std::cout<<";\n\n  // Reactions:";
-	      for(int ic=0; ic<reactionsSize; ic++){
-                std::cout <<"\n  "<<  reactionsk->get(ic)->getIdAttribute()<<": ";
-		int reactSize=reactionsk->get(ic)->getNumReactants();
-		int prodSize=reactionsk->get(ic)->getNumProducts();
-		//std::cout<<"\n "<<reactSize<<" "<<prodSize;
-		for(int ire=0;ire<reactSize;ire++)
-	        {
-	          if (ire!=0){ 
-                    std::cout<<" + ";
-		  }
-		  if (speciesk->get(reactionsk->get(ic)->getReactant(ire)->getSpecies())->getHasOnlySubstanceUnits()){
-		    std::cout<<"$";
-		  }			  
-	          std::cout << reactionsk->get(ic)->getReactant(ire)->getSpecies();
-		}
-	      	std::cout<<" => "; 
-		for(int ire=0;ire<prodSize;ire++)
-		{              
-       		  if (ire!=0){
-                    std::cout<<" + ";
-                  }
-	          if (speciesk->get(reactionsk->get(ic)->getProduct(ire)->getSpecies())->getHasOnlySubstanceUnits()){
-                    std::cout<<"$";
-                  }
+	      << "\nmodel *" << model->getId() << "()"  ///getName or getID
 
-	          std::cout << reactionsk->get(ic)->getProduct(ire)->getSpecies()<<"; ";
-		}
-		std::cout << reactionsk->get(ic)->getKineticLaw()->getFormula()<<"; ";
-              }
-              std::cout<< "\n\n  // Species initializations:";  
-              //cnt=0;
-              for(int ic=0; ic<speciesSize; ic++){
-		  if(speciesk->get(ic)->getInitialAmount()==0){
-			   std::cout <<"\n  "<<  speciesk->get(ic)->getIdAttribute()<<" = 0";
-		  }else{
-		    if(!isnan(speciesk->get(ic)->getInitialAmount())){
-                     std::cout <<"\n  "<< speciesk->get(ic)->getIdAttribute()<<" = "<<speciesk->get(ic)->getInitialAmount()<<"/"<<speciesk->get(ic)->getCompartment()<<";";
-		    }else if(!isnan(speciesk->get(ic)->getInitialConcentration())){
-			    if (speciesk->get(ic)->getInitialConcentration()==0){
-			       std::cout <<"\n  "<< speciesk->get(ic)->getIdAttribute()<<" = 0;";
-			    }else{
-                               std::cout <<"\n  "<< speciesk->get(ic)->getIdAttribute()<<" = "<<speciesk->get(ic)->getInitialConcentration()<<"/"<<speciesk->get(ic)->getCompartment()<<";";
-			    } 
-		    }		    
-                  }
-              }
-	      std::cout<< "\n\n  // Compartments initializations:";  
-              for(int ic=0; ic<compSize; ic++){
-                std::cout << "\n  "<< compartmentsk->get(ic)->getIdAttribute()<<" = "<<compartmentsk->get(ic)->getSize() <<";"; 
-                if (compartmentsk->get(ic)->getUnits()!=""){
-		   std::cout << "\n  "<< compartmentsk->get(ic)->getIdAttribute()<<" has "<<compartmentsk->get(ic)->getUnits() <<";"; 
-		}
-	      }
-
-               std::cout<< "\n\n  // Variable initializations:";  
-              for(int ic=0; ic<parametersSize; ic++){
-                std::cout << "\n  "<< parametersk->get(ic)->getIdAttribute()<<" = "<<parametersk->get(ic)->getValue() <<";";       
-                if (parametersk->get(ic)->getUnits()!=""){
-		   std::cout << "\n  "<< parametersk->get(ic)->getIdAttribute()<<" has "<<parametersk->get(ic)->getUnits() <<";";         
-                }
-	      }
+	      /// Print out Compartments and Species
+	      << "\n\n  // Compartments and Species:";  
+    for (int ic = 0; ic<compartmentsSize; ic++) {
+      std::cout << "\n  compartment " << compartmentslist->get(ic)->getIdAttribute() << ";\n";  ///getName or getID
+    } 
+    std::cout << "  species";
+    int cnt = 0;
+    for (int ic = 0; ic < speciesSize; ic++) {
+      if (!specieslist->get(ic)->getHasOnlySubstanceUnits()){      
+        if (cnt == 0){	            		
+          std::cout << " " <<  specieslist->get(ic)->getIdAttribute() << " in "<<specieslist->get(ic)->getCompartment();  
+       	} else {	             	
+          std::cout << ", " <<  specieslist->get(ic)->getIdAttribute() << " in "<<specieslist->get(ic)->getCompartment();  
+        }  
+        cnt++;
+      }
+    }
+    cnt = 0;
+    for (int ic = 0; ic < speciesSize; ic++) {
+      if (specieslist->get(ic)->getHasOnlySubstanceUnits()){
+        if (cnt == 0) { 
+          std::cout << ";\n  substanceOnly species";
+          std::cout << " $" <<  specieslist->get(ic)->getIdAttribute() << " in "<<specieslist->get(ic)->getCompartment();
+        } else {                        
+          std::cout << ", $" <<  specieslist->get(ic)->getIdAttribute() << " in "<<specieslist->get(ic)->getCompartment();
+	}	
+      	cnt++;
+      }
+    }
+  
+    /// Print out Reactions
+    std::cout << ";\n\n  // Reactions:";
+    for (int ic = 0; ic < reactionsSize; ic++) {
+      std::cout << "\n  " <<  reactionslist->get(ic)->getIdAttribute() << ": ";
+      int reactSize = reactionslist->get(ic)->getNumReactants();
+      int prodSize = reactionslist->get(ic)->getNumProducts();
+      for (int ire = 0; ire < reactSize; ire++){
+        if (ire!=0) {  
+          std::cout << " + ";
+        }
+	if (specieslist->get(reactionslist->get(ic)->getReactant(ire)->getSpecies())->getHasOnlySubstanceUnits()){
+          std::cout << "$";
+	}			  
+        std::cout << reactionslist->get(ic)->getReactant(ire)->getSpecies();
+      }
+      std::cout << " => "; 
+      for (int ire = 0; ire < prodSize; ire++) {              
+        if (ire!=0) { 
+          std::cout << " + ";
+        }
+        if (specieslist->get(reactionslist->get(ic)->getProduct(ire)->getSpecies())->getHasOnlySubstanceUnits()) {
+          std::cout << "$";
+        }
+        std::cout << reactionslist->get(ic)->getProduct(ire)->getSpecies() << "; ";
+      }
+      std::cout << reactionslist->get(ic)->getKineticLaw()->getFormula() << "; ";
+      ///std::cout << "\n "<< SBML_formulaToString( reactionslist->get(ic)->getKineticLaw()->getMath())<<"; ";
                
-              std::cout<< "\n\n  // Other declarations:"<<"\n  const ";
-              for(int ic=0; ic<compSize; ic++){
-                std::cout << compartmentsk->get(ic)->getIdAttribute();
-              }
-              for(int ic=0; ic<parametersSize; ic++){
-                std::cout <<", "<< parametersk->get(ic)->getIdAttribute();   
-              }
-	      std::cout <<";";
+      std::string formula = SBML_formulaToString(reactionslist->get(ic)->getKineticLaw()->getMath()); //char *
+      std::string toReplace("pow(");
+      std::string toReplace2(", ");
+      std::string toReplace3(")");
+      size_t pos = formula.find(toReplace);
+      while (pos < formula.length()) {
+        if (pos != std::string::npos) {
+          formula.replace(pos,toReplace.length(),"");
+        }
+        size_t pos2 = formula.find(toReplace2,pos);
+        formula.replace(pos2,toReplace2.length(),"^");
+        size_t pos3 = formula.find(toReplace3,pos);
+        formula.replace(pos3,toReplace3.length(),"");
+        pos = formula.find(toReplace);
+      }
+      std::cout << "\n  ";
+      for (int ic = 0; ic < parametersSize; ic++) {
+        std::string toFindPar(parameterslist->get(ic)->getIdAttribute());
+        size_t posPar = formula.find(toFindPar);
+        if (posPar != std::string::npos) {
+          std::cout << "var " <<parameterslist->get(ic)->getIdAttribute() << " := " << parameterslist->get(ic)->getValue() << "; ";
+        } 
+      }
+      std::cout << "m_rate := " << formula << ";\n";
 
-              std::cout<< "\n\n  // Unit definitions:";
-              for(int ic=0; ic<unitsSize; ic++){
-                std::cout << "\n  unit "<< unitsk->get(ic)->getIdAttribute();
-		if (unitsk->get(ic)->getIdAttribute()=="time"){
-			std::cout<<"_unit";
-		}
-		std::cout<<" = ";
-		ListOfUnits* unitslistk=unitsk->get(ic)->getListOfUnits();
-		int unitslistSize=unitslistk->size();
-		for(int iu=0; iu<unitslistSize;iu++){
-                  std::cout<<UnitKind_toString(unitslistk->get(iu)->getKind());	
-                  if (unitslistk->get(iu)->getExponent()>1){
-			  std::cout<<"^"<<unitslistk->get(iu)->getExponent();
-		  }
-		  std::cout<<";";
 
-		}
-              }
+      /**std::cout << "FunctionDefinition " << reactionsk->get(ic)->getIdAttribute();
+      const ASTNode*  math = reactionsk->get(ic)->getKineticLaw()->getMath();   
+      if (math->getNumChildren() > 1) {
+        std::cout<<" " << math->getNumChildren()<<"\n";
+	std::cout << "\n(" << ( math->getLeftChild() )->getName();
+        for (int n = 0; n < math->getNumChildren() - 1; ++n){
+       	  std::cout <<", " << ( math->getChild(n) )->getName();
+        }
+      }   
 
-	      std::cout<< "\n\n  // Display names:";
-              for(int ic=0; ic<unitsSize; ic++){
-                if (unitsk->get(ic)->getIdAttribute()!=unitsk->get(ic)->getName()&&unitsk->get(ic)->getName()!=""){
-                   std::cout << "\n  "<< unitsk->get(ic)->getIdAttribute()<<" is \"" <<unitsk->get(ic)->getName() <<"\";";
-                }
+      std::cout <<") := ";
+      if (math->getNumChildren() == 0){ 
+        std::cout << "(no body defined)";
+      } else {
+        math    = math->getChild(math->getNumChildren() - 1);
+        char* formula = SBML_formulaToString(math);
+        std::cout << formula ;
+      }*/
 
-	        if (unitsk->get(ic)->getIdAttribute()=="time"){
-                 // if(){        
-			std::cout<<"\n  time_unit is \""<<unitsk->get(ic)->getName()<<"\";";
-		//}
-		}
+   
+    }  
+   
+    /// Print out Species initializations
+    std::cout << "\n\n  // Species initializations:";  
+    for (int ic = 0; ic < speciesSize; ic++) {
+      if (specieslist->get(ic)->getInitialAmount() == 0) {
+        std::cout << "\n  " <<  specieslist->get(ic)->getIdAttribute() << " = 0";
+      } else {
+        if (!isnan(specieslist->get(ic)->getInitialAmount())){
+          std::cout << "\n  " << specieslist->get(ic)->getIdAttribute() << " = " << specieslist->get(ic)->getInitialAmount() << "/" <<specieslist->get(ic)->getCompartment() << ";";
+       	} else if (!isnan(specieslist->get(ic)->getInitialConcentration())) {
+          if (specieslist->get(ic)->getInitialConcentration() == 0) { 
+	    std::cout << "\n  " << specieslist->get(ic)->getIdAttribute() << " = 0;";
+	  } else {
+            std::cout << "\n  " << specieslist->get(ic)->getIdAttribute() << " = " << specieslist->get(ic)->getInitialConcentration() << "/" << specieslist->get(ic)->getCompartment() << ";";
+	  } 
+	}		
+      }
+    }
+    
+    /// Print out Compartments initializations
+    std::cout << "\n\n  // Compartments initializations:";  
+    for (int ic = 0; ic < compartmentsSize; ic++) {
+      std::cout << "\n  " << compartmentslist->get(ic)->getIdAttribute() << " = " <<compartmentslist->get(ic)->getSize() << ";"; 
+      if (compartmentslist->get(ic)->getUnits() != "") {
+        std::cout << "\n  " << compartmentslist->get(ic)->getIdAttribute() << " has " << compartmentslist->get(ic)->getUnits() << ";"; 
+      }
+    }
 
-              }
+    /// Print out Variable initializations
+    std::cout << "\n\n  // Variable initializations:";  
+    for (int ic = 0; ic < parametersSize; ic++) {
+      std::cout << "\n  " << parameterslist->get(ic)->getIdAttribute() << " = " << parameterslist->get(ic)->getValue() << ";";       
+      if (parameterslist->get(ic)->getUnits() != "") {
+        std::cout << "\n  " << parameterslist->get(ic)->getIdAttribute() << " has " << parameterslist->get(ic)->getUnits() << ";";         
+      }
+    }
+           
+    /// Print out other declarations
+    std::cout << "\n\n  // Other declarations:" << "\n  const ";
+    for (int ic = 0; ic < compartmentsSize; ic++) {
+      std::cout << compartmentslist->get(ic)->getIdAttribute();
+    }
+    for (int ic = 0; ic < parametersSize; ic++) {
+      std::cout << ", " << parameterslist->get(ic)->getIdAttribute();   
+    }
+    std::cout << ";";
 
-              for(int ic=0; ic<parametersSize; ic++){
-                if (parametersk->get(ic)->getIdAttribute()!=parametersk->get(ic)->getName()&&parametersk->get(ic)->getName()!=""){
-                   std::cout << "\n  "<< parametersk->get(ic)->getIdAttribute()<<" is \"" <<parametersk->get(ic)->getName() <<"\";";
-                }
-              }
+    /// Print out Unit definitions
+    std::cout << "\n\n  // Unit definitions:";
+    for (int ic = 0; ic < unitsSize; ic++) {
+      std::cout << "\n  unit "<< unitslist->get(ic)->getIdAttribute();
+      if (unitslist->get(ic)->getIdAttribute() == "time") {
+        std::cout << "_unit";
+      }
+      std::cout << " = ";
+      ListOfUnits* subunitslist = unitslist->get(ic)->getListOfUnits();
+      int subunitsSize = subunitslist->size();
+      //std::cout<<"\n "<<unitslistSize<<"\n";
+      for (int iu = 0; iu < subunitsSize ; iu++) {
+        if (subunitslist->get(iu)->getExponent() == -1) {
+          std::cout << "/";
+        }
+	std::cout << UnitKind_toString(subunitslist->get(iu)->getKind());	
+        if (subunitslist->get(iu)->getExponent() > 1) {
+          std::cout << "^" << subunitslist->get(iu)->getExponent();
+	}
+	if (iu == subunitsSize-1) {
+	  std::cout << ";";
+	}
+      }
+    }
 
-	    std::cout<<"\nend\n";
- 
-	     
-	      /*Konstantia*/
-              
-	      //<< std::endl;
+    /// Print out Display names
+    std::cout << "\n\n  // Display names:";
+    for (int ic = 0; ic < unitsSize; ic++) {
+      if (unitslist->get(ic)->getIdAttribute() != unitslist->get(ic)->getName() && unitslist->get(ic)->getName() != "") {
+        std::cout << "\n  " << unitslist->get(ic)->getIdAttribute() << " is \"" << unitslist->get(ic)->getName() << "\";";
+      }
+
+      if (unitslist->get(ic)->getIdAttribute() == "time") {
+        std::cout << "\n  time_unit is \"" << unitslist->get(ic)->getName() << "\";";
+      }
+    }
+
+    for (int ic = 0; ic < parametersSize; ic++) {
+      if (parameterslist->get(ic)->getIdAttribute() != parameterslist->get(ic)->getName() && parameterslist->get(ic)->getName() != ""){
+        std::cout << "\n  " << parameterslist->get(ic)->getIdAttribute() << " is \"" << parameterslist->get(ic)->getName() << "\";";
+      }
+    }
+
+    std::cout << "\nend\n";
   }
 
-  Model* model = document->getModel();
 
   if (model == NULL) {
     std::cout << "Faile to get model from " << filename << std::endl;
